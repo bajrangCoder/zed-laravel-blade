6cb93bf20e10e3370bd270f5bccf0d641d2b9696
Raunak Raj <71929976+bajrangCoder@users.noreply.github.com>
2025-02-21 18:11:57 +0530

    feat: add emmet language server

--- languages/blade/injections.scm
+++ languages/blade/injections.scm
@@ -10,9 +10,3 @@
 ((php_only) @content
     ; (#set! "language" "php_only"))
     (#set! "language" "php"))
-
-; currently removed due to some confilict
-; ((text) @content
-;     (#has-ancestor? @content "envoy")
-;     (#set! "combined")
-;     (#set! "language" "bash"))
