24e4c95c8543fe232fa45a26e60e2185d8354928
Raunak Raj <71929976+bajrangCoder@users.noreply.github.com>
2026-03-16 01:21:49 +0530

    fix: indents and update grammar

--- languages/blade/indents.scm
+++ languages/blade/indents.scm
@@ -1,11 +1,45 @@
-(directive_start) @indent.begin
+[
+  (conditional
+    (directive_start) @start
+    (directive_end) @end)
+  (loop
+    (directive_start) @start
+    (directive_end) @end)
+  (section
+    (directive_start) @start
+    (directive_end) @end)
+  (stack
+    (directive_start) @start
+    (directive_end) @end)
+  (once
+    (directive_start) @start
+    (directive_end) @end)
+  (fragment
+    (directive_start) @start
+    (directive_end) @end)
+  (envoy
+    (directive_start) @start
+    (directive_end) @end)
+  (livewire
+    (directive_start) @start
+    (directive_end) @end)
+  (switch
+    (directive_start) @start
+    (directive_end) @end)
+  (verbatim
+    (directive_start) @start
+    (directive_end) @end)
+  (php_statement
+    (directive_start) @start
+    (directive_end) @end)
+] @indent
 
-(directive_end) @indent.end
+; HTML
+(start_tag
+  ">" @end) @indent
 
-
-; html
-(start_tag ">" @end) @indent
-(self_closing_tag "/>" @end) @indent
+(self_closing_tag
+  "/>" @end) @indent
 
 (element
   (start_tag) @start
